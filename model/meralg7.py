fd7caa1df375006ae0a163e914e85e83e69f0edc
Kostis-S-Z <kostis.s.z.24@gmail.com>
2020-02-12 11:50:26 +0100

    Fix tensor dimensions

--- model/meralg7.py
+++ model/meralg7.py
@@ -64,7 +64,7 @@ class Net(nn.Module):
 
     def getBatch(self,x,y,t):
         mxi = Variable(torch.from_numpy(np.array(x))).float().view(1,-1)
-        myi = Variable(torch.from_numpy(np.array(y))).long()
+        myi = Variable(torch.from_numpy(np.array(y))).long().view(1)
         if self.cuda:
             mxi = mxi.cuda()
             myi = myi.cuda()
@@ -79,7 +79,7 @@ class Net(nn.Module):
                 k = order[j]
                 x,y,t = self.M[k]
                 xi = Variable(torch.from_numpy(np.array(x))).float().view(1,-1)
-                yi = Variable(torch.from_numpy(np.array(y))).long()
+                yi = Variable(torch.from_numpy(np.array(y))).long().view(1)
                 
                 if self.cuda:
                     xi = xi.cuda()
